a5303eda5a8a98efe8d97f4f28ba34b577db28e6
stefanofiorenza <stefanofiorenza@gmail.com>
2019-11-05 21:18:39 +0200

    [TicTacToe][S3]: added event handler and state in Board. Square receives value and function handler as props.

--- Demo/rj-100-tic-tac-toe/src/index.js
+++ Demo/rj-100-tic-tac-toe/src/index.js
@@ -3,18 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-       {this.props.value}
+        /* 4) connect event onClick to function received as property from parent Component */
+      <button className="square" onClick={() => this.props.onClick()} >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       /* 1) Array storing state for all Square' */
+      squares: Array(9).fill(null),
+    };
+  }
+
+  /**2) Replace previous array with a new one, copying all elements and overriding [i] position with 'X' */
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+     /** 3) Parent function handleClick is passed as a property*/
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
