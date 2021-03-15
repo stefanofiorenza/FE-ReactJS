5446a50b80d4f4aa6e257e261babf30af599b904
stefano.fiorenza <stefanofiorenza@gmail.com>
2021-03-15 17:28:14 +0200

    320-Redux

---
+++ Demo/rj-320-redux-auth/src/components/LoginPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+//import fakeAuth  from './AuthenticationService.js';
+
+export default class Login extends React.Component {
+
+
+    render() {
+
+       {/*
+      const { redirectToReferrer } = this.state
+    
+        if (redirectToReferrer === true) {
+            return <Redirect to='/' />
+        }
+      */}
+  
+      return (
+        <div>
+            <h2>LOGIN PAGE:</h2>
+            <p>log in to view protected pages</p>
+            <button onClick={this.login}>Log in</button>
+        </div>
+      )
+    }
+
+  }
--- Demo/rj-320-redux-auth/src/components/MasterPage.js
+++ Demo/rj-320-redux-auth/src/components/MasterPage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link, Route,Switch } from 'react-router-dom';
-
-import PrivateRoute from './PrivateRoute.jsx';
-import LoginPage from './LoginPage.jsx';
-import ProtectedPage from './ProtectedPage.jsx';
+import LoginPage from './LoginPage.js';
 
 const HomePage = (props) =>{
   return ( <div> <h3>Public. You can always access this page</h3> </div>)
@@ -21,7 +18,9 @@ const ManagerPage = (props) =>{
   return ( <div> <h3>Welcome to Manager Page</h3> </div>)
 }
 
-
+const RegistrationPage = (props) =>{
+  return ( <div> <h3>Welcome to Manager Page</h3> </div>)
+}
 function MasterPage() {
   return (
       <div>
@@ -29,15 +28,20 @@ function MasterPage() {
         <ul className="nav navbar-nav">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/login">Login</Link></li>
-          <li><Link to="/public">Public</Link></li>
-          <li><Link to="/protected">Protected</Link></li>
+          <li><Link to="/registration">Registration</Link></li>
+          <li><Link to="/admin">AdminPage</Link></li>
+          <li><Link to="/user">UserPage</Link></li>
+          <li><Link to="/manager">ManagerPage</Link></li>
         </ul>       
 
         <Switch>
-          <Route exact path="/" component={DefaultPage}/>
-          <Route path="/login" component={LoginPage}/>
-          <Route path="/public" component={PublicPage}/>         
-          <PrivateRoute path="/protected" component={ProtectedPage}/> 
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/registration" component={RegistrationPage} />
+
+          <Route path="/admin" component={AdminPage} />
+          <Route path="/user" component={UserPage} />
+          <Route path="/manager" component={ManagerPage} />
         </Switch>
       </div>
   );
